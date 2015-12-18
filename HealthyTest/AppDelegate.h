5679eacb268252cb67af336d8bfcd5fbae6790e4
fillinse <Fillinse@192.168.6.57>
2015-12-18 16:38:24 +0800

    Initial Commit

---
+++ HealthyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthyTest
+//
+//  Created by Fillinse on 15/12/18.
+//  Copyright © 2015年 Fillinse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthyTest
+//
+//  Created by Fillinse on 15/12/18.
+//  Copyright © 2015年 Fillinse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
